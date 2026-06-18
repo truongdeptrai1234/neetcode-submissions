93565b4be7c706c8835138554ac8e1c2ad7966c6
gitCoreTruong <38565830+truongdeptrai1234@users.noreply.github.com>
2026-06-19 02:38:59 +0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let compareS=[];
+        if(s.length===1) return false;
+        for(let i=0;i<s.length;i++){
+           if(s[i]==='(' || s[i]==='[' || s[i]==='{') compareS.push(s[i]);
+           else {
+            const char=compareS.pop();
+            if(Math.abs(char?.codePointAt(0)-s[i].codePointAt(0))>2 || !char) return false;
+            }
+
+        }
+        if(compareS.at(-1)==='(' || compareS.at(-1)==='[' || compareS.at(-1)==='{') return false;
+        return true
+    }
+}
