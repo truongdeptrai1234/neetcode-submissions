3dfa4a0213f23096fc9fc382ef0f6a2f029ca9cf
gitCoreTruong <38565830+truongdeptrai1234@users.noreply.github.com>
2026-07-03 19:31:23 +0700

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let leftRange=0,rightRange=matrix.length-1;
+        let targetRow=null;
+        while(leftRange<=rightRange){
+            let mid=leftRange+Math.floor((rightRange-leftRange)/2);
+            if(matrix[mid][0]<=target && matrix[mid][matrix[mid].length-1]>=target){
+                targetRow=mid;
+                leftRange=0;rightRange=matrix[mid].length-1;
+                break;
+            }
+            if(matrix[mid][0]<target) leftRange=mid+1;
+            else if(matrix[mid][0]>target) rightRange=mid-1;
+        }
+        while(leftRange<=rightRange){
+            let mid=leftRange+Math.floor((rightRange-leftRange)/2);
+            if(matrix[targetRow][mid]===target) return true;
+            if(matrix[targetRow][mid]<target) leftRange=mid+1;
+            else if(matrix[targetRow][mid]>target) rightRange=mid-1;
+        }
+        return false;
+    }
+}
