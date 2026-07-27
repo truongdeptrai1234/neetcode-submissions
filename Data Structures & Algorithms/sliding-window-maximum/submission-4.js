0429ced52c4245d65f128bb893dfcd09eecd471f
gitCoreTruong <38565830+truongdeptrai1234@users.noreply.github.com>
2026-07-28 04:47:27 +0700

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        let left=0;
+        let result=[];
+        if(k===1) return nums;
+        for(let right=0;right<nums.length;right++){
+            while(right-left+1===k){
+                let tempWindArr=nums.slice(left,right+1);
+                result.push(Math.max(...tempWindArr));
+                left++;
+            }
+        }
+        
+        return result; 
+    }
+}
