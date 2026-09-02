511b5f9cc41b915edeb111a68b2c0162df577191
gitCoreTruong <38565830+truongdeptrai1234@users.noreply.github.com>
2026-09-02 20:45:25 +0700

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        function height(node) {
+        if (!node) return 0;
+
+        const left = height(node.left);
+        if (left === -1) return -1;
+
+        const right = height(node.right);
+        if (right === -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+
+    return height(root) !== -1;
+    }
+}
